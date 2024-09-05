348607b5f0d2dbc3aacca812bb6ca0d372649ffd
GujjarlapudiSandeep <sandeepgujjarlapudi28@gmail.com>
2024-09-05 15:53:59 +0530

    practing DOM

---
+++ index.js
@@ -0,0 +1,38 @@
+function showName() {
+  alert(document.getElementById("name").value);
+}
+function showEmail() {
+  alert(document.getElementById("email").value);
+}
+function showMobile() {
+  alert(document.getElementById("mobile").value);
+}
+function showGender() {
+  alert(document.getElementById("gender").value);
+}
+
+// function setFirstName() {
+//   document.getElementById("firstname").innerText = "sandeep";
+// }
+// function setLastName() {
+//   document.getElementById("lastname").innerText = "Gujjarlapudi";
+// }
+
+// function Fullname() {
+//   document.getElementById("fullname").innerText = "SandeepGujjarlapudi";
+// }
+// function ResultDefault() {
+//   document.getElementById("firstname").innerText = "Hello";
+//   document.getElementById("lastname").innerText = "FLM";
+//   document.getElementById("fullname").innerText = "vegita";
+// }
+
+// // function submitFunction() {
+// //   let names = document.getElementById("name").value;
+// //   // console.log(names);
+// //   document.getElementById("out").innerText = names;
+// // }
+// // function getFunction() {
+// //   let gender = document.getElementById("gender").value;
+// //   document.getElementById("out1").innerText = gender;
+// // }
